69d7401cc7c9b7463c7d983ae7739c7bffb4a2d2
Frodo <102017064+Frodo-Web@users.noreply.github.com>
2022-07-29 08:35:52 +0300

    Switch to gifs

---
+++ load-gifs-to-app.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const path = require("path");
+const relativeArrayPath = './src/Application/Images.js';
+const relativeImagesPath = './docs/gifs';
+const dirname = path.resolve(relativeImagesPath);
+
+
+async function readDir(dirname) {
+    const allResults = [];
+
+    try {
+        const files = await fs.promises.readdir(dirname);
+
+        for (const fileName of files) {
+            allResults.push('./gifs/' + fileName);
+        }
+        return allResults;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+readDir(dirname).then(data => {
+    const images = JSON.stringify(data);
+    const str = 'const Images = ' + '\n' + images + ';' + '\n\n' + 'export default Images;';
+    fs.writeFile(
+        path.resolve(relativeArrayPath),
+        str,
+        function (err) {
+            if (err) {
+                console.error(err);
+            }
+        }
+    );
+});
